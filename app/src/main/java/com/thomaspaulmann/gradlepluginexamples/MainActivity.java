abcf7cdd632ce8b2f8a462285a511b5f62111967
thomaspaulmann <hello@thomaspaulmann.com>
2016-02-15 11:12:30 +0100

    Configure Documentor

--- app/src/main/java/com/thomaspaulmann/gradlepluginexamples/MainActivity.java
+++ app/src/main/java/com/thomaspaulmann/gradlepluginexamples/MainActivity.java
@@ -3,6 +3,9 @@ package com.thomaspaulmann.gradlepluginexamples;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * This is our MainActivity and we want to document this.
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
